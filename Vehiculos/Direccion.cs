b77e5dd8f7dabd9b1379275fdfeca8ea26279843
Miguel <miguel.diazv84@gmail.com>
2023-12-18 22:20:21 +0100

    Initial commit

---
+++ Vehiculos/Direccion.cs
@@ -0,0 +1,10 @@
+namespace CleanArchitecture.Domain.Vehiculos;
+
+public record Direccion
+(
+    string Calle,
+    string Departemento,
+    string Provincia,
+    string Ciudad,
+    string Pais
+);
