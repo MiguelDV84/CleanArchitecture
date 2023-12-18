a715d5357ef4a0c384d08ecc709b278c6326e95f
Miguel <miguel.diazv84@gmail.com>
2023-12-18 23:00:59 +0100

    Create Alquiler and DateRange

---
+++ Alquileres/DateRange.cs
@@ -0,0 +1,33 @@
+namespace CleanArchitecture.Domain.Alquileres;
+
+public sealed record DateRange
+{
+    private DateRange(
+        DateOnly initialDate,
+        DateOnly finishDate
+    )
+    {
+        InitialDate = initialDate;
+        FinishDate = finishDate;
+    }
+
+    public DateOnly InitialDate { get; private set; }
+    public DateOnly FinishDate { get; private set; }
+    public int Days => FinishDate.DayNumber - InitialDate.DayNumber;
+
+    public static DateRange Create(DateOnly initialDate, DateOnly finishDate)
+    {
+        if(finishDate > initialDate)
+        {
+            throw new ApplicationException("La fecha final no puede ser mayor que la fecha de inicio");
+        }
+
+        var dateRange = new DateRange(
+            initialDate,
+            finishDate
+        );
+
+        return dateRange;
+    }
+
+}
