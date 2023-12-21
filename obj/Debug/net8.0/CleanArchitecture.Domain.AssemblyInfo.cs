db4d2cf8f0bd350a0ba5c7efe629b165023ddabf
Miguel <miguel.diazv84@gmail.com>
2023-12-21 16:28:37 +0100

    Refactor moneda

--- obj/Debug/net8.0/CleanArchitecture.Domain.AssemblyInfo.cs
+++ obj/Debug/net8.0/CleanArchitecture.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c4c74964afb4fffcf44fa5656154fafdac51b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
